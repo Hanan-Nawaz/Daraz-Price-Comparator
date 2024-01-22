7a34976e06dca18d1c0e7c3bef0bad970a739307
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-01-22 19:58:23 +0500

    extract class is called in main class.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
+import sys
+sys.path.append("/Users/HananNawaz/Documents/LaptopScraper/code")
+
+from extract import extract
+
 query_parameter = input('Enter name of Laptop.... : ')
+
+extract_ = extract(query_parameter)
+extract_.extracting()
 
 print(query_parameter)
